82bc0db4dfeee648a7f2d85e9193c84cf3173a67
tangsir-alt <69881528+tangsir-alt@users.noreply.github.com>
2020-08-19 14:20:00 +0800

    Create Hello - 副本.cs

---
+++ Hello - 副本.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace name{
+    class program{
+        static void Main(string[] args){
+            //嘿嘿，我改了
+            System.Console.WriteLine("hello");
+            int x = 1;
+        }
+    }
+}
