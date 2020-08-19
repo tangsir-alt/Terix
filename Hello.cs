e0f8e80136eaa6ec55114d2943a856cb5b23a40c
TNinu <69881582+TNinu@users.noreply.github.com>
2020-08-19 14:55:14 +0800

    Update Hello.cs

--- Hello.cs
+++ Hello.cs
@@ -5,6 +5,7 @@ namespace name{
         static void Main(string[] args){
             //嘿嘿，我改了
             System.Console.WriteLine("hello");
+            //嘿嘿，我也改了
             int x = 1;
         }
     }
